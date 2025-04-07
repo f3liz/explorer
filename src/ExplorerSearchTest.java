d7ad521b453b2a628bc46133d9001b8094e17255
f3liz <felixchen14@gmail.com>
2025-04-07 13:03:16 -0700

    added some tests for possibleMoves

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -1,4 +1,9 @@
 import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class ExplorerSearchTest {
@@ -40,5 +45,44 @@ public class ExplorerSearchTest {
             ExplorerSearch.explorerLocation(island);
         });
         assertEquals("Explorer not found", exception.getMessage());
+    }
+
+    @Test
+    public void testPossibleMoves_allDirectionsOpen() {
+        int[][] enclosure = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+        int[] location = {1, 1};
+        List<int[]> moves = ExplorerSearch.possibleMoves(enclosure, location);
+        Set<String> moveSet = toSet(moves);
+
+        assertEquals(4, moves.size());
+        assertTrue(moveSet.contains("0,1")); // up
+        assertTrue(moveSet.contains("2,1")); // down
+        assertTrue(moveSet.contains("1,0")); // left
+        assertTrue(moveSet.contains("1,2")); // right
+    }
+
+    @Test
+    public void testPossibleMoves_NoDirectionsOpen() {
+        int[][] enclosure = {
+            {2, 3, 2},
+            {2, 0, 2},
+            {2, 3, 3}
+        };
+        int[] location = {1, 1};
+        List<int[]> moves = ExplorerSearch.possibleMoves(enclosure, location);
+        assertTrue(moves.isEmpty());
+    }
+
+    // from salamander search: converts int[] into string to show coordinates
+    private Set<String> toSet(List<int[]> list) {
+        Set<String> set = new HashSet<>();
+        for (int[] arr : list) {
+            set.add(arr[0] + "," + arr[1]);
+        }
+        return set;
     }
 }
